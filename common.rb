125dd45c05e6b2ebc4703b8af3fa72fbb425b92e
がむ <gam0022@gmail.com>
2012-04-30 08:16:25 +0900

    顔文字がおかしくなる不具合を仮対策。

--- common.rb
+++ common.rb
@@ -372,7 +372,7 @@ class String
 			node = node.next
 		end
 
-		buf.gsub("だのだ", "なのだ").gsub("のだよ", "のだ").gsub(/EOS$/,"").gsub(/EOS $/,"").gsub(/なのだ [.,]/, "").gsub("なのだ.なのだ", "なのだ").gsub(/(俺|私|わたし|おら)/, "僕")
+		buf.gsub("だのだ", "なのだ").gsub("のだよ", "のだ").gsub(/EOS$/,"").gsub(/EOS $/,"").gsub(/なのだ [.,]/, "").gsub("なのだ.なのだ", "なのだ").gsub(/(俺|私|わたし|おら)/, "僕").gsub(/卒/, "´")
 	end	
 
 	#
