0b77fa22cb401dc10eaba3648481ca6c1f024928
gam0022 <gam0022@gmail.com>
2012-11-08 02:46:25 +0900

    replace tab to spaces

--- follow.rb
+++ follow.rb
@@ -9,24 +9,24 @@ daigorou = TwitterBot.new
 new_follow = ( daigorou.users('follow') | Twitter.follower_ids.ids ) - Twitter.friend_ids.ids - Twitter.friendships_outgoing.ids - daigorou.users('remove')
 
 new_follow.each do |id|
-	begin
-		logs "[#{Twitter.user(id).screen_name}]をフォローします。"
-		Twitter.follow(Twitter.user(id).id)
-	rescue
-		logs "#error: #{$!}"
-	else
-		text = ["フォロー返したのだ！", "フォローありがとうなのだ！", "フォローしたのだ！"].sample
-		daigorou.post("#{Twitter.user(id).name}、#{text}", Twitter.user(id).screen_name)
-	end
+  begin
+    logs "[#{Twitter.user(id).screen_name}]をフォローします。"
+    Twitter.follow(Twitter.user(id).id)
+  rescue
+    logs "#error: #{$!}"
+  else
+    text = ["フォロー返したのだ！", "フォローありがとうなのだ！", "フォローしたのだ！"].sample
+    daigorou.post("#{Twitter.user(id).name}、#{text}", Twitter.user(id).screen_name)
+  end
 end
 
 new_unfollow = ( daigorou.users('remove') & Twitter.friend_ids.ids ) | (Twitter.friend_ids.ids - Twitter.follower_ids.ids ) - daigorou.users('follow')
 
 new_unfollow.each do |id|
-	begin
-		logs "[#{Twitter.user(id).screen_name}]をリムーブします。"
-		Twitter.unfollow(Twitter.user(id).id)
-	rescue
-		logs "#error: #{$!}"
-	end
+  begin
+    logs "[#{Twitter.user(id).screen_name}]をリムーブします。"
+    Twitter.unfollow(Twitter.user(id).id)
+  rescue
+    logs "#error: #{$!}"
+  end
 end
--- tweet.rb
+++ tweet.rb
@@ -14,7 +14,7 @@ flag = Flag.new(false, false, false)
 keyword = nil
 
 #
-#	オプション解析
+# オプション解析
 #
 
 str_update = nil
@@ -25,8 +25,8 @@ opt.on('-t', '--time') {|v| flag.time = true}
 opt.on('-w', '--weather') {|v| flag.weather = true}
 opt.on('-d', '--debug') {|v| daigorou.debug = true }
 opt.on('-k VAL', '--keyword VAL') {|v| 
-	keyword = v
-	flag.regular = true
+  keyword = v
+  flag.regular = true
 }
 opt.parse!(ARGV)
 str_update = ARGV[0]
@@ -34,60 +34,60 @@ str_update = ARGV[0]
 logs "#Debug Mode" if daigorou.debug
 
 #
-#	定期的なつぶやき
+# 定期的なつぶやき
 #
 
 if flag.regular
 
-	str_update = nil
+  str_update = nil
 
-	if !keyword
-		keywords = daigorou.get_keywords
-		stock = daigorou.get_stock
+  if !keyword
+    keywords = daigorou.get_keywords
+    stock = daigorou.get_stock
 
-		if keywords.size != 0
-			loop do
-				faild = nil
-				keyword = keywords.sample
-				stock.each do |word|
-					if keyword == word || keyword =~ /EOS$/ || keyword =~ /^\w+$/
-						faild = true
-						logs "#faild: set to keyword [#{word}]"
-						break
-					end
-				end
-				break unless faild
-			end
-		end
-	end
+    if keywords.size != 0
+      loop do
+        faild = nil
+        keyword = keywords.sample
+        stock.each do |word|
+          if keyword == word || keyword =~ /EOS$/ || keyword =~ /^\w+$/
+            faild = true
+            logs "#faild: set to keyword [#{word}]"
+            break
+          end
+        end
+        break unless faild
+      end
+    end
+  end
 
-	if keyword
-		str_update = daigorou.generate_phrase(keyword)
-		logs "keyword: [#{keyword}]"
-		daigorou.add_stock(keyword)
-	else
-		logs "#error: faild to set keyword"
-	end
+  if keyword
+    str_update = daigorou.generate_phrase(keyword)
+    logs "keyword: [#{keyword}]"
+    daigorou.add_stock(keyword)
+  else
+    logs "#error: faild to set keyword"
+  end
 
-	if !str_update
-		str_update = daigorou.config['WordsOnFaildRegularTweet'].sample
-	end
+  if !str_update
+    str_update = daigorou.config['WordsOnFaildRegularTweet'].sample
+  end
 
 end
 
 #
-#	天気
+# 天気
 #
 
 str_update = weather if flag.weather
 
 
 #
-#	ツイート
+# ツイート
 #
 
 if str_update
-	daigorou.post(str_update, false, nil, flag.time, 1)
+  daigorou.post(str_update, false, nil, flag.time, 1)
 else
-	logs "#error: faild to generate str_update"
+  logs "#error: faild to generate str_update"
 end
