fc3590e4c3012a6defe442ecb9a177d472c740a1
gam0022 <gam0022@gmail.com>
2015-02-22 17:16:37 +0900

    リプライは優先的に学習する

--- bin/response.rb
+++ bin/response.rb
@@ -292,8 +292,8 @@ daigorou.client.on_timeline_status do |status|
   end
 
   # 学習させる
-  if rand(10) == 0
-    daigorou.learn(text.filter) if !daigorou.debug && daigorou.users.config(user_id)[:learn]
+  if (rand(10) == 0 || isMention_not_RT) && !daigorou.debug && daigorou.users.config(user_id)[:learn]
+    daigorou.learn(text.filter)
   end
 
   #
